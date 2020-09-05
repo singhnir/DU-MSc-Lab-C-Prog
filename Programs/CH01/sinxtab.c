ab6423f0e3c567e751fe7577edf6bbb99c53fe49
nirmalsingh <hinduites2016@gmail.com>
2020-09-05 09:31:50 +0530

    [MANUAL]:tabulates values of sin(x)

---
+++ Programs/CH01/sinxtab.c
@@ -0,0 +1,21 @@
+// sinxtab.c - tabulates values of sin(x)
+#include <stdio.h>
+#include <math.h>
+
+main()
+{
+	float x,x1,x2,y,dx,pi;
+	int i,n=20;
+	pi = 4.0*atan(1.0);
+	printf("Enter the x1 and x2 in units of pi:\n");
+	scanf("%f, %f",&x1, &x2);
+	x1=x1*pi;
+	x2=x2*pi;
+	dx=(x2-x1)/(float)(n); //to get the intervals
+	for (int i = 0; i <=n; i++)
+	{
+		x=x1+i*dx;
+		y=sin(x);
+		printf("for x:%.2f value of sin(x)= %.2f\n", x,y );
+	}
+}
