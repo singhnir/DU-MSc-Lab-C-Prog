961031cbd7f4ce0cf2236b550e2b3407fc2dd0ae
nirmalsingh <hinduites2016@gmail.com>
2020-09-13 18:54:23 +0530

    Table of the trigonometric functions sin(x), cos(x) and tan(x).

---
+++ Excercise/CH01/trigtable.c
@@ -0,0 +1,22 @@
+// evaluating two functions sin(x) and cos(x) and storing the results in text file
+#include<stdio.h>
+#include <math.h>
+
+int main()
+{
+	float x;
+	//FILE*fp: declaring a pointer of type file
+	//NULL: to make sure the file was successfully opened
+	FILE*fp=NULL;
+
+	fp=fopen("trig.txt","w");		//w: open a file handle in write mode
+
+	// pi/4 is 0.7853981634 rad
+	for (x=0;x<=0.78; x+=0.01)
+	{
+		//print line to the file
+		fprintf(fp, "%f\t %f\t %f\t %f\n",x,sin(x),cos(x),tan(x));
+	}
+	fclose(fp); 	//close the file handle
+	return 0;
+}
