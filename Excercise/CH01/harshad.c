b652d7b25bef04797da7ee0f913648589e8db965
nirmalsingh <hinduites2016@gmail.com>
2020-09-14 09:15:13 +0530

    much clear code as a=i/10 gives only the integer value.

--- Excercise/CH01/harshad.c
+++ Excercise/CH01/harshad.c
@@ -1,4 +1,6 @@
-//to find Harshad numbers between 50 and 70 both inclusive
+/*to find Harshad numbers between 50 and 70 both inclusive
+*Some integers have a property that they are divisible by the sum of their digits. Like 84 is divisible by 8 + 4 = 12
+*We call them Harshad numbers.*/
 #include<stdio.h>
 #include <math.h>
 
@@ -8,11 +10,12 @@ int main()
 	printf("Harshad numbers between 50 and 70 both inclusive:\n");
 	for (int i=50;i<=70;++i)
 	{
-		a=i%10;		// to get the second number using remainder
-		b=(i-a)/10;		// to get the first number
+		a=i/10;		// to get the first number using quotient
+		b=i%10;		// to get the second number using remainder
 		if (i%(a+b)==0)  //checking harshad number condition
 		{
 			printf("%d\n",i);
 		}
 	}
 	return 0;
+}
