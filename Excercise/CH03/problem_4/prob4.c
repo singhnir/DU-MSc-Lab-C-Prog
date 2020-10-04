bbaa592d60f7f994877abfe02e209e61c1f47f46
nirmalsingh <hinduites2016@gmail.com>
2020-10-04 10:28:15 +0530

    program to generate datasheets for F(z)

---
+++ Excercise/CH03/problem_4/prob4.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<math.h>
+
+int main()
+{
+    FILE*fp=NULL;
+    fp=fopen("prob4.txt","w");
+
+    double z,t0,t1,R,sum,accuracy;
+    accuracy=0.00001;
+
+    for(z=0;z<=1;z=z+0.1)
+    {
+        int i=1;
+        t0=z;   //defining first term of series
+        sum=t0; //first term is also first partial sum        
+        do{
+            R=-(M_PI*M_PI*pow(z,4))/((4*i+1));
+            t1=R*t0;
+            sum=sum+t1;
+            t0=t1;
+            i++;
+        } while(fabs(t1/sum)>accuracy);
+        sum=sum*cos(M_PI*z*z/2);
+        fprintf(fp,"%lf\t%lf\n",z,sum);
+    }
+}
