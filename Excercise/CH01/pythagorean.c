262d71aaefafaa0b2f80e66ad59389fca05f4555
nirmalsingh <hinduites2016@gmail.com>
2020-10-04 10:22:40 +0530

    revised the program.

--- Excercise/CH01/pythagorean.c
+++ Excercise/CH01/pythagorean.c
@@ -1,24 +1,22 @@
 //a program to finnd all the distinct sets of Pythagorean numbers less than 100
 #include<stdio.h>
-#include <math.h>
 
 int main()
 {
-	int a,b,c,n=100;
-	printf("distinct sets of Pythagorean numbers less than 100\n");
-	printf("\na\t b\t c\n");
-	for (int a=1; a<=n;a+=1)
-	{
-		for (int b=1;b<=n;b+=1)
-		{
-			for (int c=1;c<=n;c+=1)
-			{
-				if (a*a+b*b==c*c)
-				{
-					printf("%d\t %d\t %d\n",a,b,c);
-				}
-			}
-		}
-	}
-	return 0;
+    int a,b,c,n=100;
+    printf("distinct sets of Pythagorean numbers less than 100\n");
+    printf("\na\t b\t c\n");
+    for (int a=1; a<=n;++a)
+    {
+        for (int b=a;b<=n;++b)
+        {
+            for (int c=b;c<=n;++c)
+            {
+                if (c*c==a*a+b*b)
+                {
+                    printf("%d\t %d\t %d\n",a,b,c);
+                }
+            }
+        }
+    }
 }
