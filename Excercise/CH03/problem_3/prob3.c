e5f49a3758b9025640112525fcd9369568450e73
nirmalsingh <hinduites2016@gmail.com>
2020-10-05 21:42:51 +0530

    program to generate datasheets for bessel functions

---
+++ Excercise/CH03/problem_3/prob3.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+#include<math.h>
+
+//factorial function declaration
+double factorial(int n)
+{
+    if (n>=1)
+        return n*factorial(n-1);
+    else
+        return 1;
+}
+
+int main()
+{
+    FILE*fp=NULL;
+    fp=fopen("prob3.txt","w");
+
+    double t0,t1,R,sum,x,accuracy=0.00001;
+    int n;
+    printf("Enter the value of n:");
+    scanf("%d",&n);
+
+    for(x=0;x<=10;x=x+0.1)
+    {
+        int k=1;
+        t0=1/factorial(n);
+        sum=t0;     
+        do{
+            R=-(x*x/4)/(k*(n+k));
+            t1=R*t0;
+            sum=sum+t1;
+            t0=t1;
+            k++;
+        } while(fabs(t1/sum)>accuracy);
+        sum=sum*pow(x/2,n);
+        fprintf(fp,"%lf\t%lf\n",x,sum);
+        //printf("%lf\t%lf\n",x,sum);
+    }
+     
+}
