d751f03bbd3526d635637d2284fcab786f4c55d1
nirmalsingh <hinduites2016@gmail.com>
2020-10-20 12:15:37 +0530

    fixed typo error.

--- Excercise/CH03/problem_2/prob2.c
+++ Excercise/CH03/problem_2/prob2.c
@@ -10,17 +10,17 @@ int main()
     accuracy=0.00001;
 
     for(x=0;x<=M_PI;x=x+0.01)
-    {    
+    {
         int i=1;
     	t0=1;   //defining first term of series
     	sum=t0; //first term is also first partial sum
         do{
-            R=-(x*x)/(2*i-1)/(2*i);
+            R=-(x*x)/((2*i-1)*(2*i));
             t1=R*t0;
             sum=sum+t1;
             t0=t1;
             i++;
-        } while(fabs(t1/sum)>accuracy);    
+        } while(fabs(t1/sum)>accuracy);
         fprintf(fp,"%f\t%f\n",x,sum);
     }
 }
