4428a6489f2d72e4a7427a66794e7d8c5357806b
hisacro <fsl@displ.nl>
2023-12-19 12:58:37 +0530

    problem 2 simplified

--- Excercise/CH06/problem_2/ex2.c
+++ Excercise/CH06/problem_2/ex2.c
@@ -1,18 +1,42 @@
 #include <stdio.h>
 #include <math.h>
 
+FILE *p = NULL; 
 float acc = 0.00001;
 
-// float
-// func (float x, float y)
-// {
-// 	return 0;
-// }
+/*
+ *  Mind map
+ *  --------
+ *   mathematical function
+ *     -> integrator
+ *      -> plot for integrator
+ */
+
+// integration constants
+float t0          = 0;
+float t1          = M_PI/2.0;
+float integ_steps = 500;
+
+float
+func(float x, float A)
+{
+	return ( 4/(float)( 1 - pow(sin(A/2.0),2) * pow(sin(x),2) ) );
+}
+
+float 
+func_approx(float A)
+{
+	return ( 2*M_PI*( 1 + pow((A/4.0),2)) ) ;
+}
 
 
 float 
-trapez (float func(float x, float y), float a, float b, float n, float dummy)
+trapez (float A)
 {
+	float a = t0;
+	float b = t1;
+	float n = integ_steps;
+
 	float h;
 	float sum=0;
 
@@ -20,59 +44,54 @@ trapez (float func(float x, float y), float a, float b, float n, float dummy)
 	h = (b-a)/(float)n;
 
 	for (int i = 1; i<n; i++)
-		sum+=func(a+(i*h),dummy);
+		sum+=2*func(a+(i*h),A);
 
-	return ( h/2.0*(func(a,dummy)+func(b,dummy)+2*sum) );
+	return ( h/2.0*(func(a,A)+func(b,A)+sum) );
 }
 
 
 float 
-simpson (float func(float x, float y), float a, float b, float n, float dummy)
+simpson (float A)
 {
+	float a = t0;
+	float b = t1;
+	float n = integ_steps;
+
 	float h;
 	float sum=0;
 
 	// finding step size with number of brackets = n 
-	h = (b-a)/(float)n;
+	h = (b-a)/(float)(n);
 
 	for (int i = 1; i<n; i++) {
 		if (i%2 == 0)
-			sum+=2*func(a+(i*h),dummy);
+			sum+=2*func(a+(i*h),A);
 		else
-			sum+=4*func(a+(i*h),dummy);
+			sum+=4*func(a+(i*h),A);
 	}
 
-	return ( h/3.0*(func(a,dummy)+func(b,dummy)+sum) );
+	return ( h/3.0*(func(a,A)+func(b,A)+sum) );
 }
 
-float
-t_integral(float x, float a)
-{
-	return ( 4/(float)( 1 - pow(sin(a/2.0),2) * pow(sin(x),2) ) );
-}
 
-float 
-t_approx(float a)
-{
-	return ( 2*M_PI*( 1 + pow( (a/4.0),2 ) )) ;
-}
-
-FILE *p = NULL; 
 
 int
 main()
 {
 	p = fopen("ex2.txt","w");
-	float a=0.0;
-	float b=M_PI_2;
+
 	float y1,y2,y3;
-	fprintf(p,"A \t simpson_integration \t Approximation \t error\n");
+
+	fprintf(p,"#A \t simpson_integration \t Approximation \t error\n");
 	for (float A = 0.1; A < M_PI; A=A+0.1) {
-		y1 = simpson(t_integral,a,b, 500, A);
-		y2 = t_approx(A);
-		y3 = (y1-y2)/(y1) * 100;
+
+		y1 = simpson(A);
+		y2 = func_approx(A);
+		y3 = fabs(y1-y2)/(y1) * 100; // percentage error
 
 		fprintf(p,"%f \t %f \t %f \t %f \n", A, y1, y2, y3) ;
 	}
+
+	fclose(p);
 
 }
--- Excercise/CH06/problem_2/ex2.gnu
+++ Excercise/CH06/problem_2/ex2.gnu
@@ -1,5 +1,5 @@
-set term png   
-set output  "ex2.png"
+set term postscript   
+set output  "ex2.eps"
 plot "ex2.txt" using 1:2 with lines title  "simpsons", \
  "ex2.txt" using 1:3 with lines title  "approximation", \
  "ex2.txt" using 1:4 with lines title  "error"
